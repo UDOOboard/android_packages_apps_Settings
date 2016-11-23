035f1dfbd77e8d4cd43a36cda699610a9124398c
Francesco Montefoschi <francesco.monte@gmail.com>
2016-11-23 15:57:34 +0100

    Added expansion of /data partition

--- src/org/udoo/udoosettings/UdooSettings.java
+++ src/org/udoo/udoosettings/UdooSettings.java
@@ -25,21 +25,22 @@ import java.io.OutputStream;
  * Created by harlem88 on 17/03/16.
  */
 public class UdooSettings extends PreferenceFragment {
-    private String mDisplayTypeKey;
-    private CheckBoxPreference mEnableExtOTG;
-    private static final String ENABLE_EXTERNAL_OTG = "enable_external_otg";
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
-    private static final String ADK_PROP = "persist.udoo_enable_adk";
     private Handler mUIHandler;
+    
+    private CheckBoxPreference mEnableExtOTG;
+    private static final String ADK_PROP = "persist.udoo_enable_adk";
+    
     private final static String UDOO_QUAD = "UDOO-MX6DQ";
     private final static String A62 = "A62-MX6DQ";
+    
     private static final String GOVERNORS_LIST = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
     private static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
-    private static final String NO_GOVERNOR_AVAILABLE = "no governor founds";
-    private static final String SETTINGS_AUDIO_DEVICE = "persist.audio.device";
+    private static final String NO_GOVERNORS_AVAILABLE = "No governors found";
+
     private ListPreference mAudioDevicePref;
-    private static final String KEY_AUDIO_DEVICE = "audio_device";
     private String mAudioDevice;
+    private static final String AUDIO_DEVICE_PROPERTY = "persist.audio.device";
     private static final String AUDIO_DEVICE_DEFAULT = "imx-hdmi-soc";
 
     @Override
@@ -47,20 +48,17 @@ public class UdooSettings extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.udoo_settings);
         mUIHandler = new Handler();
-        mDisplayTypeKey = getString(R.string.display_type_key);
 
         if (Build.MODEL.equals(UDOO_QUAD)) {
-            mEnableExtOTG = findAndInitCheckboxPref(ENABLE_EXTERNAL_OTG);
+            mEnableExtOTG = findAndInitCheckboxPref("udoo_enable_external_otg_key");
         }
 
         if (Build.MODEL.equals(A62)) {
-	    removePreference(getPreferenceManager().findPreference(ENABLE_EXTERNAL_OTG));
-	    removePreference(getPreferenceManager().findPreference("expand_data_partition"));
+            removePreference(getPreferenceManager().findPreference("udoo_enable_external_otg_key"));
+            removePreference(getPreferenceManager().findPreference("udoo_expand_data_partition_key"));
         }
 
-        if (Build.MODEL.equals(UDOO_QUAD) || Build.MODEL.equals(A62)) {
-            mAudioDevicePref = (ListPreference) findPreference(KEY_AUDIO_DEVICE);
-        }
+        mAudioDevicePref = (ListPreference) findPreference("udoo_audio_device_key");
 
         init();
     }
@@ -115,38 +113,36 @@ public class UdooSettings extends PreferenceFragment {
             });
         }
 
-        if (Build.MODEL.equals(UDOO_QUAD) || Build.MODEL.equals(A62)) {
-            mAudioDevicePref.setOnPreferenceChangeListener(preferenceChangeListener);
-            UtilUdoo.Get(SETTINGS_AUDIO_DEVICE, new OnResult<String>() {
-                @Override
-                public void onSuccess(String value) {
-                    mAudioDevice = value.length() > 0 ? value : AUDIO_DEVICE_DEFAULT;
+        mAudioDevicePref.setOnPreferenceChangeListener(preferenceChangeListener);
+        UtilUdoo.Get(AUDIO_DEVICE_PROPERTY, new OnResult<String>() {
+            @Override
+            public void onSuccess(String value) {
+                mAudioDevice = value.length() > 0 ? value : AUDIO_DEVICE_DEFAULT;
 
-                    mAudioDevicePref.setValue(mAudioDevice);
-                    mAudioDevicePref.setSummary(mAudioDevice);
-                }
+                mAudioDevicePref.setValue(mAudioDevice);
+                mAudioDevicePref.setSummary(mAudioDevice);
+            }
 
-                @Override
-                public void onError(Throwable throwable) {
-                    mAudioDevice = AUDIO_DEVICE_DEFAULT;
-                    mAudioDevicePref.setValue(mAudioDevice);
-                    mAudioDevicePref.setSummary(mAudioDevice);
-                }
-            });
-        }
+            @Override
+            public void onError(Throwable throwable) {
+                mAudioDevice = AUDIO_DEVICE_DEFAULT;
+                mAudioDevicePref.setValue(mAudioDevice);
+                mAudioDevicePref.setSummary(mAudioDevice);
+            }
+        });
 
         UtilUdoo.ReadParameter(new OnResult<String>() {
             @Override
             public void onSuccess(final String o) {
-                Preference preference = getPreferenceManager().findPreference(getString(R.string.display_type_key));
+                Preference preference = getPreferenceManager().findPreference(getString(R.string.udoo_video_output_key));
                 preference.setSummary(o);
-                preference.getEditor().putString(mDisplayTypeKey, o).apply();
+                preference.getEditor().putString(getString(R.string.udoo_video_output_key), o).apply();
                 preference.setOnPreferenceChangeListener(preferenceChangeListener);
             }
 
             @Override
             public void onError(Throwable throwable) {
-                Preference preference = getPreferenceManager().findPreference(getString(R.string.display_type_key));
+                Preference preference = getPreferenceManager().findPreference(getString(R.string.udoo_video_output_key));
                 preference.setSummary("-");
             }
         });
@@ -154,14 +150,14 @@ public class UdooSettings extends PreferenceFragment {
         UtilUdoo.ReadOneLine(GOVERNOR, new OnResult<String>() {
             @Override
             public void onSuccess(String o) {
-                ListPreference preference = (ListPreference) getPreferenceManager().findPreference(getString(R.string.governor_type_key));
+                ListPreference preference = (ListPreference) getPreferenceManager().findPreference(getString(R.string.udoo_governor_key));
                 preference.setValue(o);
                 preference.setSummary(o);
             }
 
             @Override
             public void onError(Throwable throwable) {
-                Preference preference = getPreferenceManager().findPreference(getString(R.string.governor_type_key));
+                Preference preference = getPreferenceManager().findPreference(getString(R.string.udoo_governor_key));
                 preference.setSummary("-");
             }
         });
@@ -169,7 +165,7 @@ public class UdooSettings extends PreferenceFragment {
         UtilUdoo.ReadOneLine(GOVERNORS_LIST, new OnResult<String>() {
             @Override
             public void onSuccess(String governors) {
-                ListPreference listPreference = (ListPreference) getPreferenceManager().findPreference(getString(R.string.governor_type_key));
+                ListPreference listPreference = (ListPreference) getPreferenceManager().findPreference(getString(R.string.udoo_governor_key));
                 if (governors.length() > 0) {
                     String[] av_govs = governors.split(" ");
                     listPreference.setEntries(av_govs);
@@ -178,49 +174,80 @@ public class UdooSettings extends PreferenceFragment {
                     listPreference.setOnPreferenceChangeListener(preferenceChangeListener);
                 } else {
                     CharSequence seq[] = new CharSequence[1];
-                    seq[0] = NO_GOVERNOR_AVAILABLE;
+                    seq[0] = NO_GOVERNORS_AVAILABLE;
                     listPreference.setEntries(seq);
                 }
             }
 
             @Override
             public void onError(Throwable throwable) {
-                Preference preference = getPreferenceManager().findPreference(getString(R.string.governor_type_key));
+                Preference preference = getPreferenceManager().findPreference(getString(R.string.udoo_governor_key));
                 preference.setSummary("-");
             }
         });
 
-        Preference button = (Preference)findPreference(getString(R.string.reboot_recovery));
+        Preference button = (Preference)findPreference(getString(R.string.udoo_reboot_recovery_key));
         button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
-                pm.reboot("recovery");
-                return true;
+              showDialog(getString(R.string.udoo_recovery_title), getString(R.string.udoo_recovery_mod_ok),
+                new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int which) {
+                     PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
+                     pm.reboot("recovery");
+                   }
+               });
+
+            return true;
             }
         });
 
-        Preference dataPartition = (Preference)findPreference(getString(R.string.expand_data_partition));
-        dataPartition.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                try {
-                    Process p = Runtime.getRuntime().exec("sh");
-                    OutputStream os = p.getOutputStream();
-                    os.write("cp /system/expand-data.zip /cache/expand-data.zip\n".getBytes());
-                    os.write("mkdir -p /cache/recovery/\n".getBytes());
-                    os.write("echo 'boot-recovery' >/cache/recovery/command\n".getBytes());
-                    os.write("echo '--update_package=/cache/expand-data.zip' >> /cache/recovery/command\n".getBytes());
-                    os.flush();
-                } catch (IOException ex) {
-                    return false;
+        if (!Build.MODEL.equals(A62)) {
+            Preference dataPartition = (Preference)findPreference(getString(R.string.udoo_expand_data_partition_key));
+            dataPartition.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    showDialog(getString(R.string.udoo_expand_data_partition_dialog), getString(R.string.udoo_expand_data_partition_mod_ok),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                expandDataPartition();
+                            }
+                        }
+                    );
+                    return true;
                 }
-
-                PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
-                pm.reboot("recovery");
-                return true;
+            });
+        }
+    }
+    
+    private void expandDataPartition() {
+        boolean success = false;
+        if (UtilUdoo.ExecuteCommandLine("su -c expand-data-partition")) {
+            try {
+                Process p = Runtime.getRuntime().exec("sh");
+                OutputStream os = p.getOutputStream();
+                os.write("cp /system/etc/expand-data-fs.zip /cache/expand-data-fs.zip\n".getBytes());
+                os.write("mkdir -p /cache/recovery/\n".getBytes());
+                os.write("echo 'boot-recovery' >/cache/recovery/command\n".getBytes());
+                os.write("echo '--update_package=/cache/expand-data-fs.zip' >> /cache/recovery/command\n".getBytes());
+                os.flush();
+                success = true;
+            } catch (IOException ex) {
+                return;
             }
-        });
+            
+            PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
+            pm.reboot("recovery");
+        }
+        
+        if (!success) {
+            mUIHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getActivity(), getString(R.string.udoo_governor_mod_ko), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     private Preference.OnPreferenceChangeListener preferenceChangeListener = new Preference.OnPreferenceChangeListener() {
@@ -229,7 +256,7 @@ public class UdooSettings extends PreferenceFragment {
             final String value = (String) newValue;
             final String pref_key = preference.getKey();
 
-            if (pref_key.equals(getString(R.string.display_type_key))) {
+            if (pref_key.equals(getString(R.string.udoo_video_output_key))) {
                 UtilUdoo.WriteParameter(value, new OnResult<Boolean>() {
                     @Override
                     public void onSuccess(Boolean isWritePreference) {
@@ -237,16 +264,16 @@ public class UdooSettings extends PreferenceFragment {
                             mUIHandler.post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    showRebootDialog(getString(R.string.udoo_settings_dialog_title), getString(R.string.apply_video_mod_ok));
+                                    showRebootDialog(getString(R.string.udoo_video_output_title), getString(R.string.udoo_video_output_mod_ok));
                                     preference.setSummary(value);
-                                    preference.getEditor().putString(mDisplayTypeKey, value).apply();
+                                    preference.getEditor().putString(getString(R.string.udoo_video_output_key), value).apply();
                                 }
                             });
                         } else {
                             mUIHandler.post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(getActivity(), getString(R.string.apply_governor_mod_ko), Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getActivity(), getString(R.string.udoo_video_output_mod_ko), Toast.LENGTH_LONG).show();
                                 }
                             });
                         }
@@ -257,32 +284,32 @@ public class UdooSettings extends PreferenceFragment {
                         getActivity().runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(getActivity(), getString(R.string.apply_video_mod_ko), Toast.LENGTH_LONG).show();
+                                Toast.makeText(getActivity(), getString(R.string.udoo_video_output_mod_ko), Toast.LENGTH_LONG).show();
                             }
                         });
                     }
                 });
-            } else if (pref_key.equals(getString(R.string.governor_type_key))) {
+            } else if (pref_key.equals(getString(R.string.udoo_governor_key))) {
 
                 if (UtilUdoo.ExecuteCommandLine("su -c echo " + value + " > " + GOVERNOR)) {
                     preference.setSummary(value);
                     mUIHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getActivity(), getString(R.string.apply_governor_mod_ok), Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity(), getString(R.string.udoo_governor_mod_ok), Toast.LENGTH_LONG).show();
                         }
                     });
                 } else {
                     mUIHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getActivity(), getString(R.string.apply_governor_mod_ko), Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity(), getString(R.string.udoo_governor_mod_ko), Toast.LENGTH_LONG).show();
                         }
                     });
                 }
-            } else if (KEY_AUDIO_DEVICE.equals(pref_key)) {
+            } else if (pref_key.equals(getString(R.string.udoo_audio_device_key))) {
                 if (!mAudioDevice.equals(value)) {
-                    UtilUdoo.Set(SETTINGS_AUDIO_DEVICE, value, new OnResult<Boolean>() {
+                    UtilUdoo.Set(AUDIO_DEVICE_PROPERTY, value, new OnResult<Boolean>() {
                         @Override
                         public void onSuccess(Boolean result) {
                             if (result) {
@@ -290,8 +317,8 @@ public class UdooSettings extends PreferenceFragment {
                                 mUIHandler.post(new Runnable() {
                                     @Override
                                     public void run() {
+                                        showRebootDialog(getString(R.string.udoo_audio_device_title), getString(R.string.udoo_audio_device_mod_ok));
                                         preference.setSummary(mAudioDevice);
-                                        Toast.makeText(getActivity(), R.string.audio_device_reboot_message, Toast.LENGTH_SHORT).show();
                                     }
                                 });
 
@@ -299,7 +326,7 @@ public class UdooSettings extends PreferenceFragment {
                                 mUIHandler.post(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Toast.makeText(getActivity(), R.string.audio_device_set_error, Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getActivity(), R.string.udoo_audio_device_mod_ko, Toast.LENGTH_SHORT).show();
                                     }
                                 });
 
@@ -311,7 +338,7 @@ public class UdooSettings extends PreferenceFragment {
                             mUIHandler.post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(getActivity(), R.string.audio_device_set_error, Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getActivity(), R.string.udoo_audio_device_mod_ko, Toast.LENGTH_SHORT).show();
                                 }
                             });
                         }
@@ -329,7 +356,7 @@ public class UdooSettings extends PreferenceFragment {
 
         if (pref == null) {
             throw new IllegalArgumentException("Cannot find preference with key = " + key);
-        } else if (key.equals(ENABLE_EXTERNAL_OTG)) {
+        } else if (key.equals("udoo_enable_external_otg_key")) {
             pref.setChecked(false);
         }
         mAllPrefs.add(pref);
@@ -357,5 +384,16 @@ public class UdooSettings extends PreferenceFragment {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
-}
 
+    private void showDialog(String title, String message, DialogInterface.OnClickListener listener){
+        new AlertDialog.Builder(getContext())
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.yes, listener)
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
